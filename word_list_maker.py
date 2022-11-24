fb1b97bf32f21832c1fb8ac98997db43583e786e
Christopher Rojer <christopher.rojer@gmail.com>
2022-11-24 15:19:03 +0100

    started pdf reader

--- word_list_maker.py
+++ word_list_maker.py
@@ -1,2 +1,33 @@
-word_list_maker.py
+import os
 
+
+def bdo_file_read():
+
+	for x in os.listdir():
+		if x.endswith(".pdf"):
+			filename = x
+
+
+	with open('bdo.pdf', 'r') as file:
+		data = file.readlines()
+		
+		i = 0
+		for line in data:
+			if not (i < 38):
+				print(line)
+			i += 1
+			if i == 50:
+				break
+
+
+
+
+
+
+
+def main():
+	bdo_file_read()
+
+
+if __name__ == "__main__":
+	main()
